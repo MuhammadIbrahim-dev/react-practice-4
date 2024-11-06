76d46d76aa469ee9ce799ee6aabf862b2bff043a
ibrahim-develop <muhammadibrahimmughal35@gmail.com>
2024-11-06 01:31:32 -0800

    state property full practice and logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let[istatus,setistatus]=useState(false)
+  let[menuStatus,setmenuStatus]=useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='App'>
+    <button className='micon' onClick={()=>setmenuStatus(!menuStatus)}>
+      {
+      menuStatus ? <span className='cross'>&times;</span> : <span> &#9776; </span>}</button>
+    <div className={`menu ${menuStatus ? 'activeMenu' : ''}`}>
+    <ul>
+      <li>Home</li>  
+      <li>about</li> 
+      <li>Blog</li> 
+      <li>contact</li> 
+      <li>projects</li> 
+      <li>product</li>   
+    </ul>
     </div>
+    
+        <input className='input___page' type={istatus ?'text':'password'}placeholder='Enter your password'/>
+    <button onClick={()=>{setistatus(!istatus)}}>{(istatus)?'hide':'show'}</button>
+    </div>
+
   );
 }
 
